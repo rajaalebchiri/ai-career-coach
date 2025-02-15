9f63f602ea161a2afe954fb2cea19cc7bd5e2e1e
rajaalebchiri <rajaa.lebchiri@gmail.com>
2025-02-15 21:49:39 +0100

    integrating inngest

---
+++ components/LandingPage/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Hero = () => {
+    return (
+      <main className="max-w-3xl justify-center text-center">
+        {" "}
+        <div className="">
+          <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+            Land Your Dream Job with AI-Powered Career Coaching
+          </h1>
+        </div>
+      </main>
+    );
+}
+
+export default Hero
